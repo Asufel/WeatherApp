60ab230554496622f7a68ac370d0631df253500a
Paweł Leszko <asufel2345@gmail.com>
2020-10-27 14:32:24 +0100

    Updates

---
+++ precache-manifest.d037d757d701b5de995738c0062db57b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abb51f9a0643d0ebced9d1f81f7def56",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "1bbe5a631eecae8181c6",
+    "url": "/WeatherApp/static/css/main.0eb3c40e.chunk.css"
+  },
+  {
+    "revision": "d732a7e0cf200b79b5cd",
+    "url": "/WeatherApp/static/js/2.4d5f7f3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherApp/static/js/2.4d5f7f3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bbe5a631eecae8181c6",
+    "url": "/WeatherApp/static/js/main.fcb1158e.chunk.js"
+  },
+  {
+    "revision": "a06af8027d41ac1853c9",
+    "url": "/WeatherApp/static/js/runtime-main.10e420e3.js"
+  }
+]);
